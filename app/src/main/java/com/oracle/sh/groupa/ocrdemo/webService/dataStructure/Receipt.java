9912c5dcc801a97f7d42024f254fd1e8e395c298
Alfred Shi <memoriedsea@gmail.com>
2015-01-08 10:47:44 +0800

    add dataStructure for webService

---
+++ app/src/main/java/com/oracle/sh/groupa/ocrdemo/webService/dataStructure/Receipt.java
@@ -0,0 +1,152 @@
+
+package service.webservice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>receipt complex type的 Java 类。
+ * 
+ * <p>以下模式片段指定包含在此类中的预期内容。
+ * 
+ * <pre>
+ * &lt;complexType name="receipt">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="time" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="title" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="transId" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="url" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "receipt", propOrder = {
+    "id",
+    "time",
+    "title",
+    "transId",
+    "url"
+})
+public class Receipt {
+
+    protected int id;
+    protected String time;
+    protected String title;
+    protected int transId;
+    protected String url;
+
+    /**
+     * 获取id属性的值。
+     * 
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * 设置id属性的值。
+     * 
+     */
+    public void setId(int value) {
+        this.id = value;
+    }
+
+    /**
+     * 获取time属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getTime() {
+        return time;
+    }
+
+    /**
+     * 设置time属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setTime(String value) {
+        this.time = value;
+    }
+
+    /**
+     * 获取title属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * 设置title属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setTitle(String value) {
+        this.title = value;
+    }
+
+    /**
+     * 获取transId属性的值。
+     * 
+     */
+    public int getTransId() {
+        return transId;
+    }
+
+    /**
+     * 设置transId属性的值。
+     * 
+     */
+    public void setTransId(int value) {
+        this.transId = value;
+    }
+
+    /**
+     * 获取url属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * 设置url属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setUrl(String value) {
+        this.url = value;
+    }
+
+}
---
+++ app/src/main/java/com/oracle/sh/groupa/ocrdemo/webService/dataStructure/User.java
@@ -0,0 +1,279 @@
+
+package service.webservice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>user complex type的 Java 类。
+ * 
+ * <p>以下模式片段指定包含在此类中的预期内容。
+ * 
+ * <pre>
+ * &lt;complexType name="user">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="account" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="bank" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="contact" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="costCenter" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="email" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="empId" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="manId" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="pwd" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "user", propOrder = {
+    "account",
+    "bank",
+    "contact",
+    "costCenter",
+    "email",
+    "empId",
+    "id",
+    "manId",
+    "name",
+    "pwd"
+})
+public class User {
+
+    protected String account;
+    protected String bank;
+    protected String contact;
+    protected String costCenter;
+    protected String email;
+    protected int empId;
+    protected int id;
+    protected int manId;
+    protected String name;
+    protected String pwd;
+
+    /**
+     * 获取account属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAccount() {
+        return account;
+    }
+
+    /**
+     * 设置account属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAccount(String value) {
+        this.account = value;
+    }
+
+    /**
+     * 获取bank属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getBank() {
+        return bank;
+    }
+
+    /**
+     * 设置bank属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setBank(String value) {
+        this.bank = value;
+    }
+
+    /**
+     * 获取contact属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getContact() {
+        return contact;
+    }
+
+    /**
+     * 设置contact属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setContact(String value) {
+        this.contact = value;
+    }
+
+    /**
+     * 获取costCenter属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCostCenter() {
+        return costCenter;
+    }
+
+    /**
+     * 设置costCenter属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCostCenter(String value) {
+        this.costCenter = value;
+    }
+
+    /**
+     * 获取email属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * 设置email属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEmail(String value) {
+        this.email = value;
+    }
+
+    /**
+     * 获取empId属性的值。
+     * 
+     */
+    public int getEmpId() {
+        return empId;
+    }
+
+    /**
+     * 设置empId属性的值。
+     * 
+     */
+    public void setEmpId(int value) {
+        this.empId = value;
+    }
+
+    /**
+     * 获取id属性的值。
+     * 
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * 设置id属性的值。
+     * 
+     */
+    public void setId(int value) {
+        this.id = value;
+    }
+
+    /**
+     * 获取manId属性的值。
+     * 
+     */
+    public int getManId() {
+        return manId;
+    }
+
+    /**
+     * 设置manId属性的值。
+     * 
+     */
+    public void setManId(int value) {
+        this.manId = value;
+    }
+
+    /**
+     * 获取name属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置name属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * 获取pwd属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPwd() {
+        return pwd;
+    }
+
+    /**
+     * 设置pwd属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPwd(String value) {
+        this.pwd = value;
+    }
+
+}
