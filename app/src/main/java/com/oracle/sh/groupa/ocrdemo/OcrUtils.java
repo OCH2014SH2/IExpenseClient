924451b4bd94aa4d02a3f1434d6cd3c7b73666b9
Eric YU <lin.yu@oracle.com>
2015-01-08 09:57:05 +0800

    eric branch first commit

--- app/src/main/java/com/oracle/sh/groupa/ocrdemo/OcrUtils.java
+++ app/src/main/java/com/oracle/sh/groupa/ocrdemo/OcrUtils.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
