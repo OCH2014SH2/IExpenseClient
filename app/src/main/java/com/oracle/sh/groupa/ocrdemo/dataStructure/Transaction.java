40e17af7ae5a71699b1fe8d93b0eb92d9403784d
Alfred Shi <memoriedsea@gmail.com>
2015-01-08 13:10:39 +0800

    add Transaction.java

--- app/src/main/java/com/oracle/sh/groupa/ocrdemo/dataStructure/Transaction.java
+++ app/src/main/java/com/oracle/sh/groupa/ocrdemo/dataStructure/Transaction.java
@@ -10,11 +10,113 @@ public class Transaction {
     private ArrayList<ReceiptInfo> receiptInfos;
     private double totalPrice;
     private User applicant;
-    private TransactionType transactionType;
-    private String expirData;
+    private TransactionType type;
+    private String expiredDate;
+    private TransactionStatus status;
+    private String justification;
+    private User approver;
 
-    public enum TransactionType{
+    public Transaction() {
 
     }
-    
+
+    public Transaction(String dateTime, double totalPrice,
+                       User applicant, TransactionType type, String expiredDate,
+                       TransactionStatus status, String justification, User approver) {
+        this.dateTime = dateTime;
+        this.receiptInfos = new ArrayList<>();
+        this.totalPrice = totalPrice;
+        this.applicant = applicant;
+        this.type = type;
+        this.expiredDate = expiredDate;
+        this.status = status;
+        this.justification = justification;
+        this.approver = approver;
+    }
+
+    public enum TransactionType {
+        Taxi, Meals, Accommodation, Training, Social, ClubCost
+    }
+
+    public enum TransactionStatus {
+        Pending, Approved, Denied
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public ArrayList<ReceiptInfo> getReceiptInfos() {
+        return receiptInfos;
+    }
+
+    public void setReceiptInfos(ArrayList<ReceiptInfo> receiptInfos) {
+        this.receiptInfos = receiptInfos;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public User getApplicant() {
+        return applicant;
+    }
+
+    public void setApplicant(User applicant) {
+        this.applicant = applicant;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public String getExpiredDate() {
+        return expiredDate;
+    }
+
+    public void setExpiredDate(String expiredDate) {
+        this.expiredDate = expiredDate;
+    }
+
+    public TransactionStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TransactionStatus status) {
+        this.status = status;
+    }
+
+    public String getJustification() {
+        return justification;
+    }
+
+    public void setJustification(String justification) {
+        this.justification = justification;
+    }
+
+    public User getApprover() {
+        return approver;
+    }
+
+    public void setApprover(User approver) {
+        this.approver = approver;
+    }
+
+    public void addReceiptinfo(ReceiptInfo receiptInfo) {
+        this.receiptInfos.add(receiptInfo);
+        this.totalPrice += receiptInfo.getPrice();
+
+    }
 }
