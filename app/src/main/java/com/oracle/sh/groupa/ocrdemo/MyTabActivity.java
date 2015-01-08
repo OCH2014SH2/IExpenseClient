ec51b2cb50bcd0b9ff1a570a7eeb68dfb4c8a458
johnson <johnson.s.shi@oracle.com>
2015-01-09 00:56:19 +0800

    tab....

--- app/src/main/java/com/oracle/sh/groupa/ocrdemo/MyTabActivity.java
+++ app/src/main/java/com/oracle/sh/groupa/ocrdemo/MyTabActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.TabHost;
 
@@ -16,12 +17,21 @@ public class MyTabActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        tabHost=(TabHost)findViewById(R.id.tabHost);
-        //LayoutInflater.from(this).inflate(R.layout.tab, tabHost.getTabContentView(), true);
-        //tabHost= (TabHost) findViewById(R.id.tabHost);
-        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("Reimburse").setContent(new Intent(this,ReimburseActivity.class)));
-        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("Query").setContent(new Intent(this,QueryActivity.class)));
-        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("Info").setContent(new Intent(this,InfoActivity.class)));
         setContentView(R.layout.tab);
-    }
+        try {
+            tabHost = (TabHost)findViewById(R.id.tabhost);
+            //tabHost=(TabHost)findViewById(R.id.tabHost);
+            tabHost.setup();
+
+            //LayoutInflater.from(this).inflate(R.layout.tab, tabHost.getTabContentView(), true);
+            //tabHost= (TabHost) findViewById(R.id.tabHost);
+            tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("Reimburse").setContent(new Intent(this, ReimburseActivity.class)));
+            tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("Query").setContent(new Intent(this, QueryActivity.class)));
+            tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("Info").setContent(new Intent(this, InfoActivity.class)));
+            tabHost.setCurrentTab(0);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            Log.d("EXCEPTION", ex.getMessage());
+        }
+}
 }
--- app/src/main/java/com/oracle/sh/groupa/ocrdemo/QueryActivity.java
+++ app/src/main/java/com/oracle/sh/groupa/ocrdemo/QueryActivity.java
@@ -6,9 +6,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import com.oracle.sh.groupa.ocrdemo.dataStructure.LocalTransaction;
+import com.oracle.sh.groupa.ocrdemo.webService.ExpenseManager;
+import com.oracle.sh.groupa.ocrdemo.webService.dataStructure.Transaction;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by lliyu on 1/8/2015.
@@ -17,26 +21,27 @@ public class QueryActivity extends Activity {
     String user_id = "22641";
     public Button button;
     public ListView listview;
+    ExpenseManager manager;
+    List<LocalTransaction> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.query);
         listview=(ListView)findViewById(R.id.listView);
-
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-
+                //List<LocalTransaction>  list=manager.queryAllTransactStatus(user_id);
             }
 
         });
-        ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String, Object>>();
+       /* ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String, Object>>();
         for (int j = 0; j < list.size(); j++) {
             HashMap<String, Object> map = new HashMap<String, Object>();
-            map.put("date", list.get(j).getIngredient());
-            map.put("price", list.get(j).getIngredient());
+            map.put("date", list.get(j).getDate());
+            map.put("price", list.get(j).get);
             map.put("type", list.get(j).getIngredient());
             map.put("status", list.get(j).getIngredient());
             listItem.add(map);
@@ -50,6 +55,6 @@ public class QueryActivity extends Activity {
                 new int[] {R.id.textview01,R.id.textview02,R.id.textview03,R.id.textview03,R.id.textview04}
         );
         //添加并且显示
-        listview.setAdapter(listItemAdapter);
+        listview.setAdapter(listItemAdapter); */
     }
 }
