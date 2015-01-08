2c12ac26611b366aaffa1ef0592b1f851023e3d9
johnson <johnson.s.shi@oracle.com>
2015-01-08 23:12:22 +0800

    .......

--- app/src/main/java/com/oracle/sh/groupa/ocrdemo/InfoActivity.java
+++ app/src/main/java/com/oracle/sh/groupa/ocrdemo/InfoActivity.java
@@ -2,13 +2,18 @@ package com.oracle.sh.groupa.ocrdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 /**
  * Created by lliyu on 1/8/2015.
  */
 public class InfoActivity extends Activity{
+    public ListView listview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+        listview=(ListView)findViewById(R.id.listView);
+
     }
 }
--- app/src/main/java/com/oracle/sh/groupa/ocrdemo/QueryActivity.java
+++ app/src/main/java/com/oracle/sh/groupa/ocrdemo/QueryActivity.java
@@ -2,13 +2,54 @@ package com.oracle.sh.groupa.ocrdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by lliyu on 1/8/2015.
  */
 public class QueryActivity extends Activity {
+    String user_id = "22641";
+    public Button button;
+    public ListView listview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.query);
+        listview=(ListView)findViewById(R.id.listView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        });
+        ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String, Object>>();
+        for (int j = 0; j < list.size(); j++) {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("date", list.get(j).getIngredient());
+            map.put("price", list.get(j).getIngredient());
+            map.put("type", list.get(j).getIngredient());
+            map.put("status", list.get(j).getIngredient());
+            listItem.add(map);
+        }
+        //生成适配器的Item和动态数组对应的元素
+        SimpleAdapter listItemAdapter = new SimpleAdapter(this,listItem,//数据源
+                R.layout.query_list_items,//ListItem的XML实现
+                //动态数组与ImageItem对应的子项
+                new String[] {"date","price","type","status"},
+                //ImageItem的XML文件里面的一个ImageView,两个TextView ID
+                new int[] {R.id.textview01,R.id.textview02,R.id.textview03,R.id.textview03,R.id.textview04}
+        );
+        //添加并且显示
+        listview.setAdapter(listItemAdapter);
     }
 }
