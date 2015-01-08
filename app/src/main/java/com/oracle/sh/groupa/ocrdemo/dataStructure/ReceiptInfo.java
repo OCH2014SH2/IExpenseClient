d179a22a547703b460ae95e19ea2c709f6c6208f
Alfred Shi <memoriedsea@gmail.com>
2015-01-08 10:15:41 +0800

    dataStructure.java

--- app/src/main/java/com/oracle/sh/groupa/ocrdemo/dataStructure/ReceiptInfo.java
+++ app/src/main/java/com/oracle/sh/groupa/ocrdemo/dataStructure/ReceiptInfo.java
@@ -4,4 +4,37 @@ package com.oracle.sh.groupa.ocrdemo.dataStructure;
  * Created by Alfred on 15/1/8.
  */
 public class ReceiptInfo {
+    private String title;
+    private String dateTime;
+    private double price;
+
+    public ReceiptInfo(String title, String dateTime, double price) {
+        this.title = title;
+        this.dateTime = dateTime;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
 }
